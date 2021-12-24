6ad903d6e8f2830448007a41ba03a9ee4e723773
Androoideka <andrej@gasic.rs>
2021-12-24 05:27:42 +0100

    Initial commit

---
+++ src/main/java/rs/raf/agasic218rn/domaci3beagasic218rn/Domaci3beAgasic218rnApplication.java
@@ -0,0 +1,13 @@
+package rs.raf.agasic218rn.domaci3beagasic218rn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Domaci3beAgasic218rnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Domaci3beAgasic218rnApplication.class, args);
+	}
+
+}
