6a501d9f6996718fa28ac0e4a6c20893177ad2e2
Andrej Gasic <andrej@gasic.rs>
2022-11-30 21:52:13 +0000

    Fix minor issues with database communication
    Rename user table since user is a reserved word in postgres
    Fix extension of properties file for portfolio profile
    Fix missing annotation for named parameter in custom query

--- src/main/java/io/github/androoideka/vm_manager/model/User.java
+++ src/main/java/io/github/androoideka/vm_manager/model/User.java
@@ -5,6 +5,7 @@ import java.util.HashSet;
 import java.util.Set;
 
 @Entity
+@Table(name = "tenancy_user")
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
--- src/main/java/io/github/androoideka/vm_manager/repositories/MachineRepository.java
+++ src/main/java/io/github/androoideka/vm_manager/repositories/MachineRepository.java
@@ -4,6 +4,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -14,5 +15,5 @@ public interface MachineRepository extends JpaRepository<Machine, Long>, JpaSpec
     @Modifying
     @Query("update Machine m set m.opCounter = m.opCounter + 1 where m.machineId = :machineId and m.version = m.opCounter")
     @Transactional
-    Integer incrementOpCounter(Long machineId);
+    Integer incrementOpCounter(@Param("machineId") Long machineId);
 }
